956faf3cfe9c52372cdb2ffac6a726b9a82fedd0
ltphong2003 <queanh150874@gmail.com>
2016-08-30 20:04:35 +0700

    Add files via upload

---
+++ js10/index.js
@@ -0,0 +1,13 @@
+<html>
+<head> 
+<script>
+
+</script>
+</head>
+<body>
+<input type="text" id="a"></input>
+<input type="text" id="b"></input>
+<input type="button" id="btn"></input>
+
+</body>
+</html>
